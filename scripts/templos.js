99d4c8049740ae0dbca27b3372899071129a5836
Juan Jaron <Juanjaron05@gmail.com>
2026-09-25 20:21:57 -0300

    Add temple album

---
+++ scripts/templos.js
@@ -0,0 +1,21 @@
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("#navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✕";
+        menuButton.setAttribute("aria-label", "Close navigation menu");
+    } else {
+        menuButton.textContent = "☰";
+        menuButton.setAttribute("aria-label", "Open navigation menu");
+    }
+});
+
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+document.querySelector("#lastModified").textContent =
+    `Last Modification: ${document.lastModified}`;
